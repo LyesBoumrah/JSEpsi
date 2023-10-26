dea8602f05539c1741f986e27cd698ceaa8ef5b6
lyes boumrah <lyes.boumrah.etu@univ-lille.fr>
2023-10-26 11:57:30 +0200

    deuxieme commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import CallToActionWithAnnotation from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        zertyiuoijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+      <CallToActionWithAnnotation/>
+       
       </main>
     </>
   )
